6767ffc5724816a34c621704a36784a3bb29c417
Durvesh <durveshmore2003@gmail.com>
2022-12-10 01:02:22 +0530

    Progress in Login Page

--- lib/login.dart
+++ lib/login.dart
@@ -15,7 +15,7 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.deepPurple[300],
+      backgroundColor: Colors.deepPurple[400],
       appBar: AppBar(
         title: Text('Tata Motors'),
         centerTitle: true,
@@ -30,78 +30,81 @@ class _LoginState extends State<Login> {
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Container(
-                  child: Text(
-                    'Login Page',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold),
-                  ),
+                Text(
+                  'Login Page',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold),
                 ),
                 SizedBox(height: 20),
                 Container(
                   child: selected == 'manager'
-                      ? Container(
-                          child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              GFButton(
-                                onPressed: () {},
-                                text: 'Manager Login',
-                                textColor: Colors.white,
-                                color: Colors.deepPurple.shade700,
-                                shape: GFButtonShape.square,
-                                type: GFButtonType.outline2x,
-                                size: GFSize.LARGE,
-                                padding: EdgeInsets.only(left: 150, right: 150),
-                              ),
-                              GFButton(
-                                onPressed: () {
+                      ? Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            GFButton(
+                              onPressed: () {},
+                              text: 'Manager Login',
+                              textColor: Colors.white,
+                              color: Colors.deepPurple.shade700,
+                              shape: GFButtonShape.square,
+                              type: GFButtonType.outline2x,
+                              size: GFSize.LARGE,
+                              hoverColor: Colors.deepPurple.shade500,
+                              padding: EdgeInsets.only(left: 150, right: 150),
+                            ),
+                            GFButton(
+                              onPressed: () {
+                                setState(() {
                                   selected = 'employee';
-                                  Login();
-                                },
-                                text: 'Employee Login',
-                                color: Colors.deepPurple.shade700,
-                                shape: GFButtonShape.square,
-                                size: GFSize.LARGE,
-                                padding: EdgeInsets.only(left: 150, right: 150),
-                              ),
-                            ],
-                          ),
+                                });
+                              },
+                              text: 'Employee Login',
+                              color: Colors.deepPurple.shade700,
+                              shape: GFButtonShape.square,
+                              size: GFSize.LARGE,
+                              padding: EdgeInsets.only(left: 150, right: 150),
+                            ),
+                          ],
                         )
-                      : Container(
-                          child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              GFButton(
-                                onPressed: () {
+                      : Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            GFButton(
+                              onPressed: () {
+                                setState(() {
                                   selected = 'manager';
-                                },
-                                text: 'Manager Login',
-                                color: Colors.deepPurple.shade700,
-                                shape: GFButtonShape.square,
-                                size: GFSize.LARGE,
-                                padding: EdgeInsets.only(left: 150, right: 150),
-                              ),
-                              GFButton(
-                                onPressed: () {
-                                  selected = 'employee';
-                                },
-                                text: 'Employee Login',
-                                textColor: Colors.white,
-                                color: Colors.deepPurple.shade700,
-                                shape: GFButtonShape.square,
-                                type: GFButtonType.outline2x,
-                                size: GFSize.LARGE,
-                                padding: EdgeInsets.only(left: 150, right: 150),
-                              ),
-                            ],
-                          ),
+                                });
+                              },
+                              text: 'Manager Login',
+                              color: Colors.deepPurple.shade700,
+                              shape: GFButtonShape.square,
+                              size: GFSize.LARGE,
+                              padding: EdgeInsets.only(left: 150, right: 150),
+                            ),
+                            GFButton(
+                              onPressed: () {},
+                              text: 'Employee Login',
+                              textColor: Colors.white,
+                              color: Colors.deepPurple.shade700,
+                              hoverColor: Colors.deepPurple.shade500,
+                              shape: GFButtonShape.square,
+                              type: GFButtonType.outline2x,
+                              size: GFSize.LARGE,
+                              padding: EdgeInsets.only(left: 150, right: 150),
+                            ),
+                          ],
                         ),
                 ),
+                GFTextFieldSquared(
+                    backgroundcolor: Colors.white,
+                    editingbordercolor: Colors.white,
+                    idlebordercolor: Colors.white,
+                    borderwidth: 1,
+                    hintText: 'Enter ManagerName')
               ],
             ),
           ),
