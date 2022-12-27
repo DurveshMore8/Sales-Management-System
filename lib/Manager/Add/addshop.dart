275353490c87c6150805adaac0e450784716ffad
Durvesh <durveshmore2003@gmail.com>
2022-12-27 16:55:18 +0530

    Shop Completed Successfully

---
+++ lib/Manager/Add/addshop.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_const_constructors_in_immutables, library_private_types_in_public_api, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class AddShop extends StatefulWidget {
+  AddShop({Key? key}) : super(key: key);
+
+  @override
+  _AddShopState createState() => _AddShopState();
+}
+
+class _AddShopState extends State<AddShop> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/Manager/Update/updateshop.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_const_constructors_in_immutables, library_private_types_in_public_api, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class UpdateShop extends StatefulWidget {
+  UpdateShop({Key? key}) : super(key: key);
+
+  @override
+  _UpdateShopState createState() => _UpdateShopState();
+}
+
+class _UpdateShopState extends State<UpdateShop> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
