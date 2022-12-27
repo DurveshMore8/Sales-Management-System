99bb20388d2711645619a852df1a15c2a4002a4a
Durvesh <durveshmore2003@gmail.com>
2022-12-27 13:55:29 +0530

    Product Completed Successfully

---
+++ lib/Manager/Add/addproduct.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_const_constructors_in_immutables, library_private_types_in_public_api, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class AddProduct extends StatefulWidget {
+  AddProduct({Key? key}) : super(key: key);
+
+  @override
+  _AddProductState createState() => _AddProductState();
+}
+
+class _AddProductState extends State<AddProduct> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/Manager/Update/updateproduct.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_const_constructors_in_immutables, library_private_types_in_public_api, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class UpdateProduct extends StatefulWidget {
+  UpdateProduct({Key? key}) : super(key: key);
+
+  @override
+  _UpdateProductState createState() => _UpdateProductState();
+}
+
+class _UpdateProductState extends State<UpdateProduct> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
