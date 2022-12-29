c1bbee5ea2e67ac7eb10dbad17080667befc69d5
Durvesh <durveshmore2003@gmail.com>
2022-12-29 10:21:02 +0530

    Update Employee Validation Completed

--- lib/Manager/Update/updateemployee.dart
+++ lib/Manager/Update/updateemployee.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: library_private_types_in_public_api, prefer_const_constructors_in_immutables, prefer_const_constructors, use_build_context_synchronously
 
 import 'package:age_calculator/age_calculator.dart';
+import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:getwidget/getwidget.dart';
@@ -15,24 +16,25 @@ class UpdateEmployee extends StatefulWidget {
 }
 
 class _UpdateEmployeeState extends State<UpdateEmployee> {
-  var name = TextEditingController();
-  var username = TextEditingController();
-  var phone = TextEditingController();
-  var emailid = TextEditingController();
-  var dob = TextEditingController();
+  //name, username, phone, emailid, dob, branchname
+  List<TextEditingController> controllers =
+      List.generate(6, (index) => TextEditingController());
+  List<String> error = ['', '', '', '', '', ''];
   int age = -1;
-  String gender = '';
+  String gender = 'a';
+  bool valid = true;
 
   @override
   void initState() {
     super.initState();
-    name.text = EmployeeState.updateEmployee['Name'];
-    username.text = EmployeeState.updateEmployee['Username'];
-    phone.text = EmployeeState.updateEmployee['Phone'];
-    emailid.text = EmployeeState.updateEmployee['EmailId'];
+    controllers[0].text = EmployeeState.updateEmployee['Name'];
+    controllers[1].text = EmployeeState.updateEmployee['Username'];
+    controllers[2].text = EmployeeState.updateEmployee['Phone'];
+    controllers[3].text = EmployeeState.updateEmployee['EmailId'];
     gender = EmployeeState.updateEmployee['Gender'];
-    dob.text = EmployeeState.updateEmployee['DateofBirth'];
-    age = AgeCalculator.age(DateTime.parse(dob.text)).years;
+    controllers[4].text = EmployeeState.updateEmployee['DateofBirth'];
+    age = AgeCalculator.age(DateTime.parse(controllers[4].text)).years;
+    controllers[5].text = EmployeeState.updateEmployee['BranchName'];
   }
 
   @override
@@ -66,13 +68,13 @@ class _UpdateEmployeeState extends State<UpdateEmployee> {
                 ),
               ],
             ),
-            SizedBox(height: 100),
+            SizedBox(height: 30),
             Column(
               children: [
                 SizedBox(
                   width: 600,
                   child: TextField(
-                    controller: name,
+                    controller: controllers[0],
                     decoration: InputDecoration(
                       fillColor: Colors.white,
                       filled: true,
@@ -83,19 +85,39 @@ class _UpdateEmployeeState extends State<UpdateEmployee> {
                           color: Colors.deepPurple.shade500,
                           fontSize: 18,
                           fontWeight: FontWeight.bold),
+                      errorText: error[0] == 'empty'
+                          ? 'Name Can\'t be empty'
+                          : error[0] == 'minimum'
+                              ? 'Minimum Limit is not Reached'
+                              : error[0] == 'maximum'
+                                  ? 'Maximum Limit is Exceeded'
+                                  : null,
                       prefixIcon: Icon(Icons.text_format_outlined),
                       prefixIconColor: Colors.deepPurple.shade500,
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.horizontal(),
                       ),
                     ),
+                    onChanged: (value) {
+                      setState(() {
+                        if (value.isNotEmpty) {
+                          if (value.length < 8) {
+                            error[0] = 'minimum';
+                          } else if (value.length > 30) {
+                            error[0] = 'maximum';
+                          } else {
+                            error[0] = '';
+                          }
+                        }
+                      });
+                    },
                   ),
                 ),
-                SizedBox(height: 20),
+                error[0] == '' ? SizedBox(height: 34) : SizedBox(height: 10),
                 SizedBox(
                   width: 600,
                   child: TextField(
-                    controller: username,
+                    controller: controllers[1],
                     readOnly: true,
                     decoration: InputDecoration(
                       fillColor: Colors.white,
@@ -107,19 +129,39 @@ class _UpdateEmployeeState extends State<UpdateEmployee> {
                           color: Colors.deepPurple.shade500,
                           fontSize: 18,
                           fontWeight: FontWeight.bold),
+                      errorText: error[1] == 'empty'
+                          ? 'Name Can\'t be empty'
+                          : error[1] == 'minimum'
+                              ? 'Minimum Limit is not Reached'
+                              : error[1] == 'maximum'
+                                  ? 'Maximum Limit is Exceeded'
+                                  : null,
                       prefixIcon: Icon(Icons.supervised_user_circle),
                       prefixIconColor: Colors.deepPurple.shade500,
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.horizontal(),
                       ),
                     ),
+                    onChanged: (value) {
+                      setState(() {
+                        if (value.isNotEmpty) {
+                          if (value.length < 5) {
+                            error[1] = 'minimum';
+                          } else if (value.length > 20) {
+                            error[1] = 'maximum';
+                          } else {
+                            error[1] = '';
+                          }
+                        }
+                      });
+                    },
                   ),
                 ),
-                SizedBox(height: 20),
+                error[1] == '' ? SizedBox(height: 34) : SizedBox(height: 10),
                 SizedBox(
                   width: 600,
                   child: TextField(
-                    controller: phone,
+                    controller: controllers[2],
                     inputFormatters: <TextInputFormatter>[
                       FilteringTextInputFormatter.digitsOnly,
                     ], // Only numbers can be entered
@@ -133,19 +175,33 @@ class _UpdateEmployeeState extends State<UpdateEmployee> {
                           color: Colors.deepPurple.shade500,
                           fontSize: 18,
                           fontWeight: FontWeight.bold),
+                      errorText: error[2] == 'empty'
+                          ? 'Mobile Number Can\'t be empty'
+                          : error[2] == 'invalid'
+                              ? 'Enter a 10 digit Mobile Number'
+                              : null,
                       prefixIcon: Icon(Icons.phone),
                       prefixIconColor: Colors.deepPurple.shade500,
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.horizontal(),
                       ),
                     ),
+                    onChanged: (value) {
+                      setState(() {
+                        if (value.length < 10 || value.length > 10) {
+                          error[2] = 'invalid';
+                        } else {
+                          error[2] = '';
+                        }
+                      });
+                    },
                   ),
                 ),
-                SizedBox(height: 20),
+                error[2] == '' ? SizedBox(height: 34) : SizedBox(height: 10),
                 SizedBox(
                   width: 600,
                   child: TextField(
-                    controller: emailid,
+                    controller: controllers[3],
                     decoration: InputDecoration(
                       fillColor: Colors.white,
                       filled: true,
@@ -156,15 +212,29 @@ class _UpdateEmployeeState extends State<UpdateEmployee> {
                           color: Colors.deepPurple.shade500,
                           fontSize: 18,
                           fontWeight: FontWeight.bold),
+                      errorText: error[3] == 'empty'
+                          ? 'Email Id Can\'t be empty'
+                          : error[3] == 'invalid'
+                              ? 'Invalid Email Id'
+                              : null,
                       prefixIcon: Icon(Icons.email),
                       prefixIconColor: Colors.deepPurple.shade500,
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.horizontal(),
                       ),
                     ),
+                    onChanged: (value) {
+                      setState(() {
+                        if (EmailValidator.validate(controllers[3].text)) {
+                          error[3] = '';
+                        } else {
+                          error[3] = 'invalid';
+                        }
+                      });
+                    },
                   ),
                 ),
-                SizedBox(height: 20),
+                error[3] == '' ? SizedBox(height: 34) : SizedBox(height: 10),
                 SizedBox(
                   width: 600,
                   child: Container(
@@ -235,21 +305,35 @@ class _UpdateEmployeeState extends State<UpdateEmployee> {
                     ),
                   ),
                 ),
-                SizedBox(height: 20),
+                gender == ''
+                    ? Container(
+                        width: 575,
+                        height: 34,
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          'Gender Can\'t be unselected',
+                          style:
+                              TextStyle(color: Colors.red[700], fontSize: 12),
+                          textAlign: TextAlign.start,
+                        ))
+                    : SizedBox(height: 34),
                 SizedBox(
                   width: 600,
                   child: TextField(
-                    controller: dob,
+                    controller: controllers[4],
+                    readOnly: true,
                     decoration: InputDecoration(
                       fillColor: Colors.white,
                       filled: true,
-                      hintText: 'YYYY-MM-DD',
+                      hintText: 'YYYY/MM/DD',
                       labelText: 'Date',
                       labelStyle: TextStyle(
                           backgroundColor: Colors.white,
                           color: Colors.deepPurple.shade500,
                           fontSize: 18,
                           fontWeight: FontWeight.bold),
+                      errorText:
+                          error[4] == 'empty' ? 'Date Can\'t be empty' : null,
                       prefixIcon: Icon(Icons.date_range),
                       prefixIconColor: Colors.deepPurple.shade500,
                       suffixIcon: IconButton(
@@ -257,16 +341,19 @@ class _UpdateEmployeeState extends State<UpdateEmployee> {
                           onPressed: () {
                             showDatePicker(
                               context: context,
-                              initialDate: DateTime.parse(dob.text),
+                              initialDate: DateTime.now(),
                               firstDate: DateTime(1900),
                               lastDate: DateTime(2100),
                             ).then((selectedDate) {
+                              setState(() {
+                                error[4] = '';
+                              });
                               age = AgeCalculator.age(selectedDate!).years;
                               List<String> date =
                                   selectedDate.toString().split('');
-                              dob.clear();
+                              controllers[4].clear();
                               for (int i = 0; i < 10; i++) {
-                                dob.text += date[i];
+                                controllers[4].text += date[i];
                               }
                             });
                           }),
@@ -277,26 +364,66 @@ class _UpdateEmployeeState extends State<UpdateEmployee> {
                     ),
                   ),
                 ),
+                error[4] == '' ? SizedBox(height: 34) : SizedBox(height: 10),
+                SizedBox(
+                  width: 600,
+                  child: TextField(
+                    controller: controllers[5],
+                    decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      filled: true,
+                      hintText: 'Naupada Branch',
+                      labelText: 'Branch Name',
+                      labelStyle: TextStyle(
+                          backgroundColor: Colors.white,
+                          color: Colors.deepPurple.shade500,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold),
+                      errorText: error[5] == 'empty'
+                          ? 'Branch Name Can\'t be empty'
+                          : error[5] == 'invalid'
+                              ? 'Invalid Branch Name'
+                              : null,
+                      prefixIcon: Icon(Icons.store),
+                      prefixIconColor: Colors.deepPurple.shade500,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.horizontal(),
+                      ),
+                    ),
+                    onChanged: (value) {
+                      setState(() {
+                        if (value.length < 8 || value.length > 20) {
+                          error[5] = 'invalid';
+                        } else {
+                          error[5] = '';
+                        }
+                      });
+                    },
+                  ),
+                ),
               ],
             ),
-            SizedBox(height: 50),
+            error[5] == '' ? SizedBox(height: 30) : SizedBox(height: 6),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 GFButton(
                   onPressed: () async {
                     await DB.openCon('employeeinfo');
+                    await DB.updatedata('Username', controllers[1].text, 'Name',
+                        controllers[0].text);
+                    await DB.updatedata('Username', controllers[1].text,
+                        'Phone', controllers[2].text);
+                    await DB.updatedata('Username', controllers[1].text,
+                        'EmailId', controllers[3].text);
                     await DB.updatedata(
-                        'Username', username.text, 'Name', name.text);
+                        'Username', controllers[1].text, 'Gender', gender);
+                    await DB.updatedata('Username', controllers[1].text,
+                        'DateofBirth', controllers[4].text);
                     await DB.updatedata(
-                        'Username', username.text, 'Phone', phone.text);
-                    await DB.updatedata(
-                        'Username', username.text, 'EmailId', emailid.text);
-                    await DB.updatedata(
-                        'Username', username.text, 'Gender', gender);
-                    await DB.updatedata(
-                        'Username', username.text, 'DateofBirth', dob.text);
-                    await DB.updatedata('Username', username.text, 'Age', age);
+                        'Username', controllers[1].text, 'Age', age);
+                    await DB.updatedata('Username', controllers[1].text,
+                        'BranchName', controllers[5].text);
                     await DB.closeCon();
                     Navigator.pop(context);
                   },
@@ -315,12 +442,13 @@ class _UpdateEmployeeState extends State<UpdateEmployee> {
                 GFButton(
                   onPressed: () {
                     setState(() {
-                      name.clear();
-                      username.clear();
-                      phone.clear();
-                      emailid.clear();
+                      controllers[0].clear();
+                      controllers[1].clear();
+                      controllers[2].clear();
+                      controllers[3].clear();
                       gender = '';
-                      dob.clear();
+                      controllers[4].clear();
+                      controllers[5].clear();
                     });
                   },
                   icon: Icon(
