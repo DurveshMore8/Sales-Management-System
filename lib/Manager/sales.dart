b390d5fc230cf84c88caf7ced9fab0127602bdf2
Durvesh <durveshmore2003@gmail.com>
2023-01-31 02:27:39 +0530

    Sales Design Partially Completed

--- lib/Manager/sales.dart
+++ lib/Manager/sales.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, prefer_const_constructors_in_immutables, library_private_types_in_public_api
 
 import 'package:flutter/material.dart';
+import 'package:sadms/Database/database.dart';
 
 class Sales extends StatefulWidget {
   Sales({Key? key}) : super(key: key);
@@ -10,6 +11,28 @@ class Sales extends StatefulWidget {
 }
 
 class _SalesState extends State<Sales> {
+  TextEditingController text = TextEditingController();
+  int selectedValue = 1;
+  List<Map<String, dynamic>> sales = [];
+
+  void getData() async {
+    await DB.openCon('sales');
+    List<Map<String, dynamic>> s = await DB.collection.find({
+      'Date':
+          '${DateTime.now().day}-${DateTime.now().month}-${DateTime.now().year}'
+    }).toList();
+    await DB.closeCon();
+    setState(() {
+      sales.addAll(s);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -24,6 +47,198 @@ class _SalesState extends State<Sales> {
             fontSize: 30,
           ),
         ),
+        SizedBox(height: 50),
+        DropdownButton(
+          dropdownColor: Colors.deepPurple[600],
+          style: TextStyle(color: Colors.white),
+          iconEnabledColor: Colors.white,
+          value: selectedValue,
+          items: [
+            DropdownMenuItem(
+              value: 1,
+              alignment: Alignment.center,
+              child: Text('\t\t\t\t\t\t\t\tEmployee\t\t\t\t\t\t\t\t'),
+            ),
+            DropdownMenuItem(
+                value: 2,
+                alignment: Alignment.center,
+                child: Text('\t\t\t\t\t\t\t\tProduct\t\t\t\t\t\t\t\t')),
+            DropdownMenuItem(
+                value: 3,
+                alignment: Alignment.center,
+                child: Text('\t\t\t\t\t\t\t\tBranch\t\t\t\t\t\t\t\t')),
+          ],
+          onChanged: (value) {
+            setState(() {
+              selectedValue = value!;
+              text.clear();
+            });
+          },
+        ),
+        SizedBox(height: 50),
+        selectedValue == 1
+            ? SizedBox(
+                width: 750,
+                child: TextField(
+                  controller: text,
+                  decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    filled: true,
+                    hintText: 'Durvesh More',
+                    labelText: 'Employee\'s Username',
+                    floatingLabelAlignment: FloatingLabelAlignment.center,
+                    labelStyle: TextStyle(
+                        backgroundColor: Colors.white,
+                        color: Colors.deepPurple.shade500,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold),
+                    prefixIcon: Icon(Icons.text_format_outlined,
+                        color: Colors.deepPurple.shade500),
+                    prefixIconColor: Colors.deepPurple.shade500,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.horizontal(),
+                    ),
+                  ),
+                  onChanged: (value) {
+                    setState(() {});
+                  },
+                ),
+              )
+            : selectedValue == 2
+                ? SizedBox(
+                    width: 750,
+                    child: TextField(
+                      controller: text,
+                      decoration: InputDecoration(
+                        fillColor: Colors.white,
+                        filled: true,
+                        hintText: 'Tata Nano',
+                        labelText: 'Product\'s Name',
+                        floatingLabelAlignment: FloatingLabelAlignment.center,
+                        labelStyle: TextStyle(
+                            backgroundColor: Colors.white,
+                            color: Colors.deepPurple.shade500,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold),
+                        prefixIcon: Icon(Icons.text_format_outlined,
+                            color: Colors.deepPurple.shade500),
+                        prefixIconColor: Colors.deepPurple.shade500,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.horizontal(),
+                        ),
+                      ),
+                      onChanged: (value) {
+                        setState(() {});
+                      },
+                    ),
+                  )
+                : SizedBox(
+                    width: 750,
+                    child: TextField(
+                      controller: text,
+                      decoration: InputDecoration(
+                        fillColor: Colors.white,
+                        filled: true,
+                        hintText: 'Kisan Nagar Branch',
+                        labelText: 'Branch\'s Name',
+                        floatingLabelAlignment: FloatingLabelAlignment.center,
+                        labelStyle: TextStyle(
+                            backgroundColor: Colors.white,
+                            color: Colors.deepPurple.shade500,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold),
+                        prefixIcon: Icon(Icons.text_format_outlined,
+                            color: Colors.deepPurple.shade500),
+                        prefixIconColor: Colors.deepPurple.shade500,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.horizontal(),
+                        ),
+                      ),
+                      onChanged: (value) {
+                        setState(() {});
+                      },
+                    ),
+                  ),
+        SizedBox(height: 50),
+        selectedValue == 1
+            ? Expanded(
+                child: ListView.builder(
+                  itemCount: sales.length,
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          color: Colors.deepPurple[700],
+                          height: 100,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Text(
+                                    'Name: ${sales[index]['CustomerName']}',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 17,
+                                    ),
+                                  ),
+                                  Text(
+                                    'Phone: ${sales[index]['Phone']}',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 17,
+                                    ),
+                                  ),
+                                  Text(
+                                    'EmailId: ${sales[index]['EmailId']}',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 17,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Text(
+                                    'Attended By: ${sales[index]['SaleBy']}',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 17,
+                                    ),
+                                  ),
+                                  Text(
+                                    'Total Purchase: ${sales[index]['TotalPrice']}',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 17,
+                                    ),
+                                  ),
+                                  Text(
+                                    'Quantity: ${sales[index]['TotalQuantity']}',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 17,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              )
+            : selectedValue == 2
+                ? SizedBox()
+                : SizedBox()
       ],
     );
   }
