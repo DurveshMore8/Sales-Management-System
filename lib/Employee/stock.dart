984480dbae7ea85178c1f8343a15d8ab38cb0db0
Durvesh <durveshmore2003@gmail.com>
2023-02-07 19:06:46 +0530

    Loading Added

--- lib/Employee/stock.dart
+++ lib/Employee/stock.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, prefer_const_constructors_in_immutables, library_private_types_in_public_api
 
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:getwidget/getwidget.dart';
 import 'package:sadms/Database/database.dart';
@@ -21,9 +22,13 @@ class _StockState extends State<Stock> {
   List<String> productname = [];
   List<String> branchname = [];
   List<int> quantity = [];
+  bool isLoading = true;
   bool check = false;
 
   void getData() async {
+    setState(() {
+      isLoading = true;
+    });
     productid.clear();
     productname.clear();
     branchname.clear();
@@ -44,6 +49,7 @@ class _StockState extends State<Stock> {
         branchname.add(maindata[i]['BranchName']);
         quantity.add(maindata[i]['Quantity']);
       }
+      isLoading = false;
     });
   }
 
@@ -55,179 +61,197 @@ class _StockState extends State<Stock> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        SizedBox(height: 20),
-        Text(
-          'Stock',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
-            fontSize: 30,
-          ),
-        ),
-        SizedBox(height: 50),
-        SizedBox(
-          width: 750,
-          child: TextField(
-            controller: text,
-            decoration: InputDecoration(
-              fillColor: Colors.white,
-              filled: true,
-              hintText: 'Tata Nano',
-              labelText: 'Product Name',
-              floatingLabelAlignment: FloatingLabelAlignment.center,
-              labelStyle: TextStyle(
-                  backgroundColor: Colors.white,
-                  color: Colors.deepPurple.shade500,
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold),
-              prefixIcon: Icon(Icons.text_format_outlined,
-                  color: Colors.deepPurple.shade500),
-              prefixIconColor: Colors.deepPurple.shade500,
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.horizontal(),
-              ),
-            ),
-            onChanged: (value) {
-              setState(() {
-                if (check) {
-                  data.clear();
-                  if (value == '') {
-                    data.addAll(maindata);
-                  } else {
-                    for (int i = 0; i < maindata.length; i++) {
-                      if (value.length <= maindata[i]['ProductName'].length) {
-                        String productnamestring = '';
-                        for (int j = 0; j < value.length; j++) {
-                          productnamestring =
-                              productnamestring + maindata[i]['ProductName'][j];
-                        }
-                        if (value.toLowerCase() ==
-                                productnamestring.toLowerCase() &&
-                            maindata[i]['BranchName'] ==
-                                branch[0]['BranchName']) {
-                          data.add(maindata[i]);
-                        }
-                      }
-                    }
-                  }
-                } else {
-                  data.clear();
-                  if (value == '') {
-                    data.addAll(maindata);
-                  } else {
-                    for (int i = 0; i < maindata.length; i++) {
-                      if (value.length <= maindata[i]['ProductName'].length) {
-                        String productnamestring = '';
-                        for (int j = 0; j < value.length; j++) {
-                          productnamestring =
-                              productnamestring + maindata[i]['ProductName'][j];
-                        }
-                        if (value.toLowerCase() ==
-                            productnamestring.toLowerCase()) {
-                          data.add(maindata[i]);
-                        }
-                      }
-                    }
-                  }
-                }
-              });
-            },
-          ),
-        ),
-        SizedBox(height: 50),
-        SizedBox(
-          child: ListTile(
-            title: Text('Display Stock of My Branch',
-                style: TextStyle(
+    return Scaffold(
+        backgroundColor: Colors.deepPurple.shade400,
+        body: isLoading
+            ? BackdropFilter(
+                filter: ImageFilter.blur(
+                  sigmaX: 2,
+                  sigmaY: 2,
+                ),
+                child: Center(
+                    child: CircularProgressIndicator(
                   color: Colors.white,
                 )),
-            leading: GFCheckbox(
-              value: check,
-              size: 25,
-              activeBgColor: Colors.deepPurple.shade700,
-              onChanged: (value) {
-                setState(() {
-                  check = value;
-                  data.clear();
-                  if (check) {
-                    for (int i = 0; i < maindata.length; i++) {
-                      if (maindata[i]['BranchName'] ==
-                          branch[0]['BranchName']) {
-                        data.add(maindata[i]);
-                      }
-                    }
-                  } else {
-                    data.addAll(maindata);
-                  }
-                });
-              },
-            ),
-          ),
-        ),
-        Expanded(
-          child: ListView.builder(
-            itemCount: data.length,
-            itemBuilder: (context, index) {
-              return GestureDetector(
-                onTap: () {},
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    color: Colors.deepPurple[700],
-                    height: 100,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Text(
-                              'Product Name: ${data[index]['ProductName']}',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 17,
-                              ),
-                            ),
-                            Text(
-                              'Product Id: ${data[index]['ProductId']}',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 17,
-                              ),
-                            ),
-                          ],
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Text(
-                              'Branch Name: ${data[index]['BranchName']}',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 17,
-                              ),
-                            ),
-                            Text(
-                              'Quantity: ${data[index]['Quantity']}',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 17,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
+              )
+            : Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(height: 20),
+                  Text(
+                    'Stock',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 30,
                     ),
                   ),
-                ),
-              );
-            },
-          ),
-        ),
-      ],
-    );
+                  SizedBox(height: 50),
+                  SizedBox(
+                    width: 750,
+                    child: TextField(
+                      controller: text,
+                      decoration: InputDecoration(
+                        fillColor: Colors.white,
+                        filled: true,
+                        hintText: 'Tata Nano',
+                        labelText: 'Product Name',
+                        floatingLabelAlignment: FloatingLabelAlignment.center,
+                        labelStyle: TextStyle(
+                            backgroundColor: Colors.white,
+                            color: Colors.deepPurple.shade500,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold),
+                        prefixIcon: Icon(Icons.text_format_outlined,
+                            color: Colors.deepPurple.shade500),
+                        prefixIconColor: Colors.deepPurple.shade500,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.horizontal(),
+                        ),
+                      ),
+                      onChanged: (value) {
+                        setState(() {
+                          if (check) {
+                            data.clear();
+                            if (value == '') {
+                              data.addAll(maindata);
+                            } else {
+                              for (int i = 0; i < maindata.length; i++) {
+                                if (value.length <=
+                                    maindata[i]['ProductName'].length) {
+                                  String productnamestring = '';
+                                  for (int j = 0; j < value.length; j++) {
+                                    productnamestring = productnamestring +
+                                        maindata[i]['ProductName'][j];
+                                  }
+                                  if (value.toLowerCase() ==
+                                          productnamestring.toLowerCase() &&
+                                      maindata[i]['BranchName'] ==
+                                          branch[0]['BranchName']) {
+                                    data.add(maindata[i]);
+                                  }
+                                }
+                              }
+                            }
+                          } else {
+                            data.clear();
+                            if (value == '') {
+                              data.addAll(maindata);
+                            } else {
+                              for (int i = 0; i < maindata.length; i++) {
+                                if (value.length <=
+                                    maindata[i]['ProductName'].length) {
+                                  String productnamestring = '';
+                                  for (int j = 0; j < value.length; j++) {
+                                    productnamestring = productnamestring +
+                                        maindata[i]['ProductName'][j];
+                                  }
+                                  if (value.toLowerCase() ==
+                                      productnamestring.toLowerCase()) {
+                                    data.add(maindata[i]);
+                                  }
+                                }
+                              }
+                            }
+                          }
+                        });
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 50),
+                  SizedBox(
+                    child: ListTile(
+                      title: Text('Display Stock of My Branch',
+                          style: TextStyle(
+                            color: Colors.white,
+                          )),
+                      leading: GFCheckbox(
+                        value: check,
+                        size: 25,
+                        activeBgColor: Colors.deepPurple.shade700,
+                        onChanged: (value) {
+                          setState(() {
+                            check = value;
+                            data.clear();
+                            if (check) {
+                              for (int i = 0; i < maindata.length; i++) {
+                                if (maindata[i]['BranchName'] ==
+                                    branch[0]['BranchName']) {
+                                  data.add(maindata[i]);
+                                }
+                              }
+                            } else {
+                              data.addAll(maindata);
+                            }
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: data.length,
+                      itemBuilder: (context, index) {
+                        return GestureDetector(
+                          onTap: () {},
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(
+                              color: Colors.deepPurple[700],
+                              height: 100,
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    children: [
+                                      Text(
+                                        'Product Name: ${data[index]['ProductName']}',
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 17,
+                                        ),
+                                      ),
+                                      Text(
+                                        'Product Id: ${data[index]['ProductId']}',
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 17,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    children: [
+                                      Text(
+                                        'Branch Name: ${data[index]['BranchName']}',
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 17,
+                                        ),
+                                      ),
+                                      Text(
+                                        'Quantity: ${data[index]['Quantity']}',
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 17,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ));
   }
 }
